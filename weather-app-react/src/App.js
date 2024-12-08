9e9742947294909ccfeb940e089ba30ad602b3d0
Aarushi Agarwal <132547930+Aarushi4Agarwal@users.noreply.github.com>
2024-12-08 14:39:08 +0530

    basic weather details(no css)

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,15 +1,33 @@
-import React,{useState} from "react"
-import axios from "axios"
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=12eb8c3557a3b3788e6ef25b1482f69c`;
 
-
-
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>60°F</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>12 MPH</div>
+        </div>
+      </div>
     </div>
   );
 }
