61e9da01f071833f0e4e3d453ebf81499a92d237
Aarushi Agarwal <132547930+Aarushi4Agarwal@users.noreply.github.com>
2024-12-08 14:09:13 +0530

    API added

---
+++ weather-app-react/src/App.js
@@ -0,0 +1,17 @@
+import React,{useState} from "react"
+import axios from "axios"
+
+function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=12eb8c3557a3b3788e6ef25b1482f69c`;
+
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
